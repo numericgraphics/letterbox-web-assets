c07cefef0f4f7e6f72b2d288e961e5f5a982d7da
Rinaldo <contact@numericgraphics.com>
2019-08-11 16:15:59 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+export rts from './assets/svg/play-rts.svg';
+export srf from './assets/svg/play-srf.svg';
+export rsi from './assets/svg/play-rsi.svg';
+export rtr from './assets/svg/play-rtr.svg';
+export swi from './assets/svg/play-swi.svg';

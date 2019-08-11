b0eb981ebe88c5292870f470452c9e25faca6044
Rinaldo <contact@numericgraphics.com>
2019-08-11 20:56:00 +0200

    modifications to use default export

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-export rts from './assets/svg/play-rts.svg';
-export srf from './assets/svg/play-srf.svg';
-export rsi from './assets/svg/play-rsi.svg';
-export rtr from './assets/svg/play-rtr.svg';
-export swi from './assets/svg/play-swi.svg';
+export {
+    default as rts,
+} from './assets/svg/play-rts.svg';
+
+export {
+    default as srf,
+} from './assets/svg/play-srf.svg';
+
+export {
+default as rsi,
+} from './assets/svg/play-rsi.svg';
+
+export {
+    default as rtr,
+} from './assets/svg/play-rtr.svg';
+
+export {
+    default as swi,
+} from './assets/svg/play-swi.svg';
